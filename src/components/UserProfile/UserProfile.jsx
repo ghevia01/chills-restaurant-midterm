b9090aeeb381f5328a65bea0c30d2284458d83e1
Gean Hevia <gcaarlos30@gmail.com>
2023-10-17 14:14:35 -0400

    feat: added UserProfile component and it's styles

---
+++ src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./user-profile-styles.css";
+
+const UserProfile = ({ name, profileImg }) => {
+  return (
+    <div className="user-profile-container">
+      <div className="profile-img-container">
+        <img className="profile-img" src={profileImg} alt="User Profile" />
+      </div>
+      <h3>Hello, {name}</h3>
+    </div>
+  );
+};
+
+export default UserProfile;
