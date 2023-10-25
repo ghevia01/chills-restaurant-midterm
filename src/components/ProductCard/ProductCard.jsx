4318171b97cba0ef3f2fa9b1f1087452706406fa
Gean Hevia <gcaarlos30@gmail.com>
2023-10-24 23:14:36 -0400

    fix: improved styles and fixed some minor issues

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -5,15 +5,15 @@ import "./product-card-styles.css";
 const ProductCard = ({ product }) => {
   return (
     <div className="product-card">
-      <div className="product-img">
+      <div className="product-img-container">
         <img src={product.image} alt="product" />
       </div>
-      <div className="product-info">
+      <div className="product-info-container">
         <div className="product-name-price-container">
           <h3 className="product-name">{product.name}</h3>
           <p className="product-price">${product.price}</p>
         </div>
-        <div className="product-description">
+        <div className="product-description-container">
           <p>{product.description}</p>
         </div>
       </div>
