a4617401117b41259fb09186d39dfbb1e767b5cf
Gean Hevia <gcaarlos30@gmail.com>
2023-10-25 23:39:03 -0400

    feat: created OrdersSection comp and styles

---
+++ src/components/OrdersSection/OrdersSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./orders-section-styles.css";
+
+const OrdersSection = () => {
+  return (
+    <div className="orders-section">
+      <h1 className="orders-section-title">Orders Section Coming Soon!</h1>
+    </div>
+  );
+};
+
+export default OrdersSection;
