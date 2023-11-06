4ab80f877e70bdcad49f9590937ad151d414a210
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-05 21:10:05 -0400

    adds order button to menuitem card

--- src/components/Cards/MenuItemCard/MenuItemCard.jsx
+++ src/components/Cards/MenuItemCard/MenuItemCard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
 import "./menu-item-card-styles.css";
 
+const MenuItemCard = ({ item, isOrdering, onAddToOrder }) => {
 
-const MenuItemCard = ({ item }) => {
   
+  // Function to handle add to order on button click
+  const handleAddToOrder = () => {
+    if (onAddToOrder) {
+      onAddToOrder(item);
+    }
+  };
+
   return (
     <div className="menu-item-card">
       <div className="item-img-container">
@@ -13,12 +19,19 @@ const MenuItemCard = ({ item }) => {
       <div className="item-info-container">
         <div className="item-name-price-container">
           <h3 className="item-name">{item.name}</h3>
-          <span className="item-price">{`$${item.price}`}</span>
+          <span className="item-price">${item.price}</span>
         </div>
         <div className="item-description-container">
           <p>{item.description}</p>
         </div>
       </div>
+      {isOrdering && (
+        <div className="order-button-container">
+          <button className="order-button" onClick={handleAddToOrder}>
+            Add
+          </button>
+        </div>
+      )}
     </div>
   );
 };
