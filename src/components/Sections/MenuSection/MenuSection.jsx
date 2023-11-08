99b2e3414b30d92d025b9949954f41ce1b714389
ghevia01 <ghevia.workspace@gmail.com>
2023-11-08 17:20:11 -0400

    refactor: menu section component to use custom hook for fetching data and sorting functions

--- src/components/Sections/MenuSection/MenuSection.jsx
+++ src/components/Sections/MenuSection/MenuSection.jsx
@@ -1,34 +1,21 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
+import { useFetchData } from "../../../hooks/useFetchData";
 
 import MenuList from "../../../components/Layouts/MenuList/MenuList";
 import Searchbar from "../../../components/UI/Searchbar/Searchbar";
 import TabNavigation from "../../../components/UI/TabNavigation/TabNavigation";
 
+import { API_FETCH_STATUS } from "../../../constants/apiFetchStatus";
+import { menuTabs } from "../../../constants/menuTabs";
+import { sortByIndex as sortByCategory, sortByName } from "../../../utils/sortingFunctions";
 import { getMenuItemData } from "../../../services/foodService";
 
 import "./menu-section.css";
 
-// Object to store the fetch status
-const FETCH_STATUS = {
-  PENDING: 'pending',
-  SUCCESS: 'success',
-  ERROR: 'error',
-};
-
-// Array of menu tabs
-const menuTabs = [
-  "All",
-  "Appetizers",
-  "Entrees",
-  "Sides",
-  "Desserts",
-  "Beverages",
-];
-
 const MenuSection = ({ isOrdering, onAddToOrder }) => {
 
-  // State to keep track of the fetch status
-  const [fetchStatus, setFetchStatus] = useState(FETCH_STATUS.PENDING);
+  // Hook to fetch menu items, returns an object with the fetch status, fetched data and error
+  const { data: fetchedMenuItems, fetchStatus, error } = useFetchData(getMenuItemData);
 
   // State to keep track of the selected tab
   const [selectedTab, setSelectedTab] = useState(menuTabs[0]);
@@ -36,51 +23,12 @@ const MenuSection = ({ isOrdering, onAddToOrder }) => {
   // State to store fetched menu items
   const [menuItems, setMenuItems] = useState([]);
 
-  // State to keep track of errors
-  const [error, setError] = useState(null);
-
-  // Fetch menu items when the component mounts 
+  // Update the menuItems state when the fetchedMenuItems state changes
   useEffect(() => {
-    // Variable to keep track of whether the component is mounted or not
-    let isMounted = true;
-
-    // Function to fetch the menu items
-    const fetchData = async () => {
-      setFetchStatus(FETCH_STATUS.PENDING);
-
-      try {
-        // Call the getMenuItemData function from the foodService module
-        const { result, data: fetchedMenuItems, message } = await getMenuItemData();
-
-        // If the component is not mounted, do not update the state
-        if (!isMounted) return;
-
-        // If the result is success, update the menuItems state and fetchStatus state
-        if (result === "success") {
-          setMenuItems(fetchedMenuItems);
-          setFetchStatus(FETCH_STATUS.SUCCESS);
-        } else {
-          // If the result is not success, set the error state and fetchStatus state
-          setError(message);
-          setFetchStatus(FETCH_STATUS.ERROR);
-        }
-      } catch (err) {
-        if (!isMounted) return;
-
-        // Set the error state and fetchStatus state
-        setError(err.message || 'Failed to fetch menu items.');
-        setFetchStatus(FETCH_STATUS.ERROR);
-      }
-    };
-
-    // Execute the fetchData function
-    fetchData();
-
-    return () => {
-      // When the component unmounts, set the isMounted variable to false
-      isMounted = false;
+    if (fetchStatus === API_FETCH_STATUS.SUCCESS) {
+      setMenuItems(fetchedMenuItems);
     }
-  }, []);
+  }, [fetchedMenuItems, fetchStatus]);
 
   // Function to handle the tab click
   const handleTabClick = (tab) => {
@@ -88,31 +36,35 @@ const MenuSection = ({ isOrdering, onAddToOrder }) => {
   };
 
   // Filter the items based on the selected tab
-  const filteredItems = useMemo(() => {
-    return selectedTab === "All"
-      ? menuItems.sort((a, b) => {
-        // Get the index of the categories in the menuTabs array
-        const indexA = menuTabs.indexOf(a.category);
-        const indexB = menuTabs.indexOf(b.category);
+  const filteredItems = () => {
+    let filtered = selectedTab === "All"
+      ? [...menuItems]
+      : menuItems.filter(item => item.category === selectedTab);
 
-        // Sort based on the index (this will sort the products based on the order of the categories in the menuTabs array)
-        return indexA - indexB;
-      })
-      : menuItems.filter((item) => item.category === selectedTab);
-  }, [selectedTab, menuItems]);
+    // Sort the items by category if the selected tab is "All"
+    if (selectedTab === "All") {
+      filtered.sort(sortByCategory(menuTabs));
+    }
+    
+    // Sort the items by name
+    filtered.sort(sortByName);
+
+    return filtered;
+  };
 
   // Early return for pending state
-  if (fetchStatus === FETCH_STATUS.PENDING) {
+  if (fetchStatus === API_FETCH_STATUS.PENDING) {
     return <p>Loading...</p>;
-  }
+  };
 
   // Early return for error state
-  if (fetchStatus === FETCH_STATUS.ERROR) {
+  if (fetchStatus === API_FETCH_STATUS.ERROR) {
     return <p>{error}</p>;
-  }
+  };
 
   return (
     <section className="menu-section">
+      {/* Pass the menuTabs array to the TabNavigation component and handle the tab click */}
       <TabNavigation tabs={menuTabs} onTabClick={handleTabClick} />
       <Searchbar />
       <div className="category-text-container">
@@ -120,8 +72,8 @@ const MenuSection = ({ isOrdering, onAddToOrder }) => {
           Category: <span className="category-text">{selectedTab}</span>
         </h2>
       </div>
-      <MenuList items={filteredItems} isOrdering={isOrdering} onAddToOrder={onAddToOrder} />
-
+      {/* Pass the filteredItems array to the MenuList component */}
+      <MenuList menuItems={filteredItems} isOrdering={isOrdering} onAddToOrder={onAddToOrder} />
     </section>
   );
 };
