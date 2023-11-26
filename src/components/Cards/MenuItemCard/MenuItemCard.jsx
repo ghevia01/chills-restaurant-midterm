ab496d133f7c3fda3d8a86eead4c09e75746ec16
Gean Hevia <gcaarlos30@gmail.com>
2023-11-26 17:45:08 -0400

    refactor: MenuItemCard component to handle card click event

--- src/components/Cards/MenuItemCard/MenuItemCard.jsx
+++ src/components/Cards/MenuItemCard/MenuItemCard.jsx
@@ -2,32 +2,47 @@ import React from "react";
 
 import "./menu-item-card.css";
 
-const MenuItemCard = ({ item, isOrdering, onAddToOrder }) => {
+const MenuItemCard = ({ item, isOrdering, onAddToOrder, onMenuItemClick }) => {
+  // Destructure the item object
+  const { name, description, price, image } = item;
 
   // Function to handle add to order on button click
-  const handleAddToOrder = () => {
+  const handleAddToOrder = (e) => {
+    // Prevent the event from bubbling up to the card click event
+    e.stopPropagation();
     if (onAddToOrder) {
       onAddToOrder(item);
     }
   };
-  
+
+  // Function to handle card click event to open the modal
+  const handleCardClick = (e) => {
+    // Prevent the event from bubbling up to the card click event
+    e.stopPropagation();
+    if (!isOrdering && onMenuItemClick) {
+      onMenuItemClick(item);
+    }
+  };
+
   return (
-    <div className="menu-item-card">
+    <div className="menu-item-card" onClick={handleCardClick}>
       <div className="item-img-container">
-        <img src={`data:image/jpeg;base64,${item.image}`} alt="Menu Item" />
+        <img src={`data:image/jpeg;base64,${image}`} alt={name} />
       </div>
       <div className="item-info-container">
         <div className="item-name-price-container">
-          <h3 className="item-name">{item.name}</h3>
-          <span className="item-price">{`$${item.price}`}</span>
+          <h3 className="item-name">{name}</h3>
+          <span className="item-price">{`$${price}`}</span>
         </div>
         <div className="item-description-container">
-          <p>{item.description}</p>
+          <p>{description}</p>
         </div>
       </div>
       {isOrdering && (
         <div className="order-button-container">
-          <button className="order-button" onClick={handleAddToOrder}>Add</button>
+          <button className="order-button" onClick={handleAddToOrder}>
+            Add
+          </button>
         </div>
       )}
     </div>
