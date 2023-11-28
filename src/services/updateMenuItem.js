517a06576618b2931db73410eb3775aafe5594e3
Manito09 <142824127+Manito09@users.noreply.github.com>
2023-11-28 09:58:45 -0400

    Update updateMenuItem.js

--- src/services/updateMenuItem.js
+++ src/services/updateMenuItem.js
@@ -1,3 +1,35 @@
-export const updateMenuItem = (updatedItem) => {
-    // TODO: implement menu item update logic
-}
+export const updateMenuItem = async (updatedMenuItem) => {
+
+  try {
+    const response = await API.put("/api/menu/update", updatedMenuItem);
+
+    if (response.status >= 200 && response.status < 300) {
+      return {
+        result: "success",
+        data: response.data,
+        message: null,
+      };
+    } else {
+
+      const errorMessage = response.data && response.data.message;
+      return {
+        result: "error",
+        data: null,
+        message: errorMessage || "An error occurred while updating the menu item",
+      };
+    }
+  } catch (error) {
+
+    console.error("Error in updateMenuItem:", error.message);
+
+    const serverMessage =
+      error.response && error.response.data && error.response.data.message;
+
+    return {
+      result: "error",
+      data: null,
+      message:
+        serverMessage || `An unexpected error occurred: ${error.message}`,
+    };
+  }
+};
