8c66f5f840477b92b06db3eda56df62771146d02
Gean Hevia <gcaarlos30@gmail.com>
2023-11-30 20:34:05 -0400

    feat: add getAllUsersData function to userServices.js

---
+++ src/services/userServices.js
@@ -0,0 +1,41 @@
+import API from "../axios";
+
+export const getAllUsersData = async () => {
+  try {
+    // fetch employee data from the server
+    const response = await API.get(" ");
+
+    // Check if the response is ok
+    if (response.status >= 200 && response.status < 300) {
+      return {
+        result: "success",
+        data: response.data,
+        message: null,
+      };
+    } else {
+      // Return the error message
+      const errorMessage = response.data && response.data.message;
+      return {
+        result: "error",
+        data: null,
+        message:
+          errorMessage || "An error occurred while getting employees data",
+      };
+    }
+  } catch (error) {
+    // Log the error
+    console.error("Error in getAllUsersData:", error.message);
+
+    // If the server sends back an error response, it might be available in error.response.data
+    const serverMessage =
+      error.response && error.response.data && error.response.data.message;
+
+    // Return the error message
+    return {
+      result: "error",
+      data: null,
+      message:
+        serverMessage || `An unexpected error occurred: ${error.message}`,
+    };
+  }
+};
