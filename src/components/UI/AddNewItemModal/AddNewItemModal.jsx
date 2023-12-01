94ec61295223790e68d0bc91327936c6e2a1720b
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-30 20:00:52 -0400

    refactor addMenuItems

--- src/components/UI/AddNewItemModal/AddNewItemModal.jsx
+++ src/components/UI/AddNewItemModal/AddNewItemModal.jsx
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-
 import ConfirmationModal from "../ConfirmationModal/ConfirmationModal";
 import ResultModal from "../ResultModal/ResultModal";
-
 import "./add-new-item-modal.css";
 
-const itemInitialValues = {
-  image: "",
-  name: "",
-  price: "",
-  description: "",
-  category: "Appetizers",
-  availability: "Available",
-};
-
 // Schema for edit fields validation
-const menuItemSchema = Yup.object().shape({
+const menuItemSchema = Yup.object({
   name: Yup.string()
     .max(40, "Name must be at most 40 characters long")
     .required("Name is required."),
@@ -39,49 +28,29 @@ const menuItemSchema = Yup.object().shape({
 });
 
 const AddNewItemModal = ({ onClose, onCreate }) => {
-  // State to store the uploaded image
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [modalState, setModalState] = useState({
+    showConfirmation: false,
+    result: "",
+    resultMessageHeader: "",
+    resultMessage: "",
+    showResult: false,
+  });
 
-  // State to store the confirmation modal visibility
-  const [showConfirmation, setShowConfirmation] = useState(false);
-
-  // State to store uodate result, messages, and visibility
-  const [result, setResult] = useState("");
-  const [resultMessageHeader, setResultMessageHeader] = useState("");
-  const [resultMessage, setResultMessage] = useState("");
-  const [showResult, setShowResult] = useState(false);
-
-  // Formik hook to handle the edit fields
   const formik = useFormik({
-    initialValues: itemInitialValues,
+    initialValues: {
+      image: "",
+      name: "",
+      price: "",
+      description: "",
+      category: "Appetizers",
+      availability: "Available",
+    },
     validationSchema: menuItemSchema,
     onSubmit: async (values) => {
-
-      console.log("Form values: ", values);
-
-      // Create a new FormData object
-      const formData = new FormData();
-
-      // Append image to FormData
-      if (values.image) {
-        formData.append("image", values.image);
-      }
-
-      // Append other values to FormData
-      for (const key in values) {
-        if (key !== "image") {
-          formData.append(key, values[key]);
-        }
-      }
-
-      // Call the onCreate function
       try {
-        const response = await onCreate(formData);
-        if (response && response.result === "success") {
-          handleUpdateResponse("success");
-        } else {
-          handleUpdateResponse("failed");
-        }
+        const response = await onCreate(convertToFormData(values));
+        handleUpdateResponse(response.result);
       } catch (error) {
         console.error("Error adding the new item:", error);
         handleUpdateResponse("unexpected");
@@ -89,78 +58,90 @@ const AddNewItemModal = ({ onClose, onCreate }) => {
     },
   });
 
-  // Function to reset the item state
-  const resetItemState = () => {
-    formik.resetForm();
+  const convertToFormData = (values) => {
+    const formData = new FormData();
+    Object.entries(values).forEach(([key, value]) => {
+      if (key !== "image" || value) {
+        formData.append(key, value);
+      }
+    });
+    return formData;
   };
 
-  // Function to handle the close button click
-  const handleCloseButtonClick = () => {
-    formik.resetForm();
-    onClose();
-  };
-
-  // Function to handle the save button click
-  const handleSaveAttempt = () => {
-    setShowConfirmation(true);
-  };
-
-  // Function to handle the confirm button click in the confirmation modal
-  const handleConfirmSave = () => {
-    setShowConfirmation(false);
-    formik.handleSubmit();
-    resetItemState();
-  };
-
-  // Function to handle the cancel button click in the confirmation modal
-  const handleCancelSave = () => {
-    setShowConfirmation(false);
-  };
-
-  // Function to handle the image change
   const handleImageChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      const imgFile = e.target.files[0];
-      const imageUrl = URL.createObjectURL(imgFile);
-      setUploadedImage(imageUrl);
-      formik.setFieldValue("image", imgFile);
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) =>
+        formik.setFieldValue("image", event.target.result);
+      reader.readAsBinaryString(file);
+      setUploadedImage(URL.createObjectURL(file));
     }
   };
 
-  // Function to handle the update response
-  const handleUpdateResponse = (updateResult) => {
-    if (updateResult === "success") {
-      setResult(updateResult);
-      setResultMessageHeader("Item Added Successfully!");
-      setResultMessage(
-        `${formik.values.name} was successfully added to the menu!`
-      );
-    } else if (updateResult === "failed") {
-      setResult("failed");
-      setResultMessageHeader("Error adding item");
-      setResultMessage(
-        `There was an error while adding the item to the menu, please try again.`
-      );
-    } else {
-      setResult("unexpected");
-      setResultMessageHeader("An unexpected error occurred");
-      setResultMessage(
-        `There was an unexpected error while adding the item to the menu, please try again.`
-      );
-    }
+  const handleUpdateResponse = (result) => {
+    const messages = {
+      success: [
+        "Item Added Successfully!",
+        `${formik.values.name} was successfully added to the menu!`,
+      ],
+      failed: [
+        "Error adding item",
+        "There was an error while adding the item to the menu, please try again.",
+      ],
+      unexpected: [
+        "An unexpected error occurred",
+        "There was an unexpected error while adding the item to the menu, please try again.",
+      ],
+    };
 
-    setShowResult(true);
+    const [header, message] = messages[result] || [
+      "Unknown Error",
+      "An unknown error occurred.",
+    ];
+    setModalState({
+      ...modalState,
+      showResult: true,
+      result,
+      resultMessageHeader: header,
+      resultMessage: message,
+    });
   };
+
+  const FieldWithError = ({ name, type = "text", label, as }) => (
+    <>
+      <label className="item-label">
+        {label}
+        {as === "textarea" ? (
+          <textarea
+            name={name}
+            className="edit-field"
+            {...formik.getFieldProps(name)}
+          />
+        ) : (
+          <input
+            name={name}
+            type={type}
+            className="edit-field"
+            {...formik.getFieldProps(name)}
+          />
+        )}
+      </label>
+      {formik.touched[name] && formik.errors[name] && (
+        <div className="edit-field-error-msg">{formik.errors[name]}</div>
+      )}
+    </>
+  );
 
   return (
     <div className="menu-item-modal">
       <div className="modal-content">
-        <form className="item-modal-form">
+        <form className="item-modal-form" onSubmit={formik.handleSubmit}>
           <div className="modal-header">
             {uploadedImage && (
               <img
-                src={uploadedImage || ""}
-                alt={`${formik.values.name}`}
+                src={uploadedImage}
+                alt={formik.values.name}
                 className="item-modal-image"
               />
             )}
@@ -169,7 +150,6 @@ const AddNewItemModal = ({ onClose, onCreate }) => {
             </label>
             <input
               id="image-upload"
-              className="edit-image-upload"
               name="image"
               type="file"
               accept="image/*"
@@ -177,116 +157,53 @@ const AddNewItemModal = ({ onClose, onCreate }) => {
             />
           </div>
           <div className="item-modal-details">
-            <div className="item-modal-name-price">
-              <label className="item-label">
-                Name:
-                <input
-                  name="name"
-                  type="text"
-                  className="edit-field"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                />
-                <div className="edit-field-error-msg">
-                  {formik.errors.name && formik.touched.name
-                    ? formik.errors.name
-                    : ""}
-                </div>
-              </label>
-              <label className="item-label">
-                Price:
-                <input
-                  name="price"
-                  type="text"
-                  className="edit-field"
-                  value={formik.values.price}
-                  onChange={formik.handleChange}
-                />
-                <div className="edit-field-error-msg">
-                  {formik.errors.price && formik.touched.price
-                    ? formik.errors.price
-                    : ""}
-                </div>
-              </label>
-            </div>
-
-            <div className="item-modal-description">
-              <label className="item-label">Description: </label>
-              <div className="edit-field-wrapper">
-                <textarea
-                  name="description"
-                  className="edit-field"
-                  value={formik.values.description}
-                  onChange={formik.handleChange}
-                />
-                <div className="edit-field-error-msg">
-                  {formik.errors.description && formik.touched.description
-                    ? formik.errors.description
-                    : ""}
-                </div>
-              </div>
-            </div>
-
-            <div className="item-modal-category">
-              <label className="item-label">Category: </label>
-              <select
-                name="category"
-                className="edit-field"
-                value={formik.values.category}
-                onChange={formik.handleChange}
-              >
-                <option value="Appetizers">Appetizers</option>
-                <option value="Entrees">Entrees</option>
-                <option value="Sides">Sides</option>
-                <option value="Desserts">Desserts</option>
-                <option value="Beverages">Beverages</option>
-              </select>
-            </div>
-
-            <div className="item-modal-availability">
-              <label className="item-label">Availability: </label>
-              <select
-                name="availability"
-                className="edit-field"
-                value={formik.values.availability}
-                onChange={formik.handleChange}
-              >
-                <option value="Available">Available</option>
-                <option value="Unavailable">Unavailable</option>
-              </select>
-            </div>
+            <FieldWithError name="name" label="Name:" />
+            <FieldWithError name="price" label="Price:" />
+            <FieldWithError
+              name="description"
+              label="Description:"
+              as="textarea"
+            />
+            <FieldWithError name="category" label="Category:" as="select" />
+            <FieldWithError
+              name="availability"
+              label="Availability:"
+              as="select"
+            />
           </div>
-
           <div className="item-modal-actions">
             <button
               className="modal-action-button modal-cancel-button"
               type="button"
-              onClick={handleCloseButtonClick}
+              onClick={onClose}
             >
               Close
             </button>
             <button
-              type="button"
               className="modal-action-button modal-save-button"
-              onClick={handleSaveAttempt}
+              type="button"
+              onClick={() =>
+                setModalState({ ...modalState, showConfirmation: true })
+              }
             >
               Add New Item
             </button>
           </div>
         </form>
-
         <ConfirmationModal
-          isOpen={showConfirmation}
-          onCancel={handleCancelSave}
-          onConfirm={handleConfirmSave}
+          isOpen={modalState.showConfirmation}
+          onCancel={() =>
+            setModalState({ ...modalState, showConfirmation: false })
+          }
+          onConfirm={formik.handleSubmit}
           message="Are you sure you want to save these changes?"
         />
         <ResultModal
-          isOpen={showResult}
-          result={result}
-          messageHeader={resultMessageHeader}
-          message={resultMessage}
-          onClose={() => setShowResult(false)}
+          isOpen={modalState.showResult}
+          result={modalState.result}
+          messageHeader={modalState.resultMessageHeader}
+          message={modalState.resultMessage}
+          onClose={() => setModalState({ ...modalState, showResult: false })}
         />
       </div>
     </div>
